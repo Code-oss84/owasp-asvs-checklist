cc999a51f664f9d01078824fcdf5d369d413ca2a
unknown <abdeAIT>
2026-02-22 17:55:50 +0000

    Initial commit: OWASP ASVS Security Checklist with Angular 21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
   production: true,
-  geminiApiKey: 'AIzaSyDDhKn-F_8Gwj62hPijhQQ_tZHjrWWFN4Q',
+  geminiApiKey: '',
   geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent'
 };
